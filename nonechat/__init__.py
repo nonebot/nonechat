b8b4b0f0f47eceaafc85cf079c9011fba9d99c3e
RF-Tar-Railt <3165388245@qq.com>
2023-07-15 01:43:47 +0800

    :bookmark: version 0.1.1

--- nonechat/__init__.py
+++ nonechat/__init__.py
@@ -7,4 +7,4 @@ from .message import Markdown as Markdown
 from .message import ConsoleMessage as ConsoleMessage
 from .setting import ConsoleSetting as ConsoleSetting
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
